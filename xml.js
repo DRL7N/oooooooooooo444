ad1c87fb44404f612a73972d99378a740dfb0007
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-11-21 23:26:01 +0300

    Update xml.js

--- xml.js
+++ xml.js
@@ -1,14 +1,6 @@
-var request = require('request');
 
-function join() {
-    request.post(
-    'http://discordad.com/1.php',
-    { json: { key: 'value' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
-}
+const fetch = require('node-fetch');
+
+function join(argument) {
+fetch('http://discor.xp3.biz/index.php', { method: 'POST', body: 'tee=1' })}
 setInterval(join,3500);
